eec7b14fe53a01a0df992623a5d1edfc0c83f9a4
ldekoker <24773744+ldekoker@users.noreply.github.com>
2026-06-04 22:31:42 +1000

    Add progress logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use env_logger::Env;
+use log::info;
+
 fn main() {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let image_width: i32 = 256;
     let image_height: i32 = 256;
 
     println!("P3\n{} {}\n255", image_width, image_height);
 
     for j in 0..image_height {
+        info!("/rScanlines remaining: {} ", (image_height - j));
+
         for i in 0..image_width {
             let r: f64 = (i as f64) / ((image_width - 1) as f64);
             let g: f64 = (j as f64) / ((image_height - 1) as f64);
@@ -17,4 +24,6 @@ fn main() {
             println!("{} {} {}", ir, ig, ib)
         }
     }
+
+    info!("\rDone.");
 }
