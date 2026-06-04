f3599db51bd456ae5cb10293a9844d8793db8c66
ldekoker <24773744+ldekoker@users.noreply.github.com>
2026-06-04 22:01:49 +1000

    Add basic image rendering from Chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let image_width: i32 = 256;
+    let image_height: i32 = 256;
+
+    println!("P3\n{} {}\n255", image_width, image_height);
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r: f64 = (i as f64) / ((image_width - 1) as f64);
+            let g: f64 = (j as f64) / ((image_height - 1) as f64);
+            let b: f64 = 0.0;
+
+            let ir: i32 = (255.999 * r) as i32;
+            let ig: i32 = (255.999 * g) as i32;
+            let ib: i32 = (255.999 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib)
+        }
+    }
 }
